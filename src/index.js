3f01eeb1e2067e08a33babfb0559386c3878b8a7
artiq75 <artiq75@icloud.com>
2023-02-28 21:49:45 +0100

    Add marker on map

--- src/index.js
+++ src/index.js
@@ -1,4 +1,48 @@
-import App from './App'
+import maplibregl from 'maplibre-gl'
 import './css/index.scss'
 
-App.start()
+const form = document.getElementById('dashboard-form')
+
+const map = new maplibregl.Map({
+  container: 'map',
+  style: 'https://demotiles.maplibre.org/style.json',
+  center: [2, 47],
+  zoom: 4
+})
+
+map.on('click', function (e) {
+  form.elements['latitude'].value = e.lngLat.lat
+  form.elements['longitude'].value = e.lngLat.lng
+})
+
+form.addEventListener('submit', function (e) {
+  e.preventDefault()
+  const formData = new FormData(form)
+  if (isEmpty(formData.values())) return
+  document.dispatchEvent(
+    new CustomEvent('event.submit', {
+      detail: Object.fromEntries(formData)
+    })
+  )
+  form.reset()
+})
+
+document.addEventListener('event.submit', function (e) {
+  const data = e.detail
+  new maplibregl.Marker().setLngLat([data.longitude, data.latitude]).addTo(map)
+})
+
+function isEmpty(args) {
+  for (const arg of args) {
+    if (!arg) return true
+  }
+  return false
+}
+
+// function getHtml(data) {
+//   return (
+//     `<h2>${data.get('title')}</h2>` +
+//     `<p>${data.get('description')}</p>` +
+//     `<button class="event-remove-btn" data-id="${data.get('id')}">Supprimer</button>`
+//   )
+// }
